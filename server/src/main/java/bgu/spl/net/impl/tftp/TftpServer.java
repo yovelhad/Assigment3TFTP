8d0a6c2e82c2420fb0134081ddf146e472de20d0
yovalhad <yovelhad@post.bgu.ac.il>
2024-03-11 15:48:00 +0200

    tftp server changed. now runnable

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -3,27 +3,40 @@ package bgu.spl.net.impl.tftp;
 import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.HashMap;
+import java.util.Set;
 
-public class TftpServer implements Connections<T> {
+public class TftpServer implements Connections<byte[]> {
+
     HashMap<Integer, ConnectionHandler> clientsMap;
     DatagramSocket socket;
-    public TftpServer(){
+    int port;
+    public TftpServer(int port){
         clientsMap = new HashMap<>();
-        int port = Integer.parseInt(args[0]);
-        socket = new DatagramSocket(port);
+        this.port = port;
+        try {
+            this.socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
     @Override
-    public void connect(int connectionId, ConnectionHandler<T> handler) {
+    public void connect(int connectionId, ConnectionHandler<byte[]> handler) {
         clientsMap.put(connectionId, handler);
     }
 
     @Override
-    public boolean send(int connectionId, T msg) {
+    public boolean send(int connectionId, byte[] msg) {
         ConnectionHandler currentClient = clientsMap.get(connectionId);
-        socket.send(T);
-
+        if(currentClient!=null) {
+            currentClient.send(msg);
+            return true;
+        }
         return false;
     }
 
@@ -33,7 +46,40 @@ public class TftpServer implements Connections<T> {
     }
 
     //TODO: Implement this
-    public void send(T msg){
+    public void send(byte[] msg){
+        Set<Integer> keys = clientsMap.keySet();
+        for(Integer ID: keys){
+            ConnectionHandler currentClient = clientsMap.get(ID);
+            currentClient.send(msg);
+        }
+    }
+
+    @Override
+    public void setSoTimeout(int connectionId, int timeOut) {
+
+    }
+
+    @Override
+    public void receive(int connectionId, DatagramPacket datagramPacket) {
+
+    }
+    public void start(){
+        while(true){
+
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        // 1. Get the address (host:port) of the server
+        String serverIP = args[0];  // 132.4.5.6
+        int serverPort = Integer.parseInt(args[1]); // 7000
+        Socket socket = new Socket(serverIP, serverPort);
+        String msg = "Hello World";
+        for (byte b : msg.getBytes()){
+            socket.getOutputStream().write(b);
+        socket.getOutputStream().write('0');  // indicate end of message (for the case of TCP)
+        socket.close();
+        }
 
     }
 
