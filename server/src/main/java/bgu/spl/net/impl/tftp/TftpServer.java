e8efe1fdf8ec6e3dbc57a7c9324103830f0ecf51
OkabiDor <dorokabi@gmail.com>
2024-03-11 11:10:04 +0200

    server Interface

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -1,5 +1,40 @@
 package bgu.spl.net.impl.tftp;
 
-public class TftpServer {
+import bgu.spl.net.srv.ConnectionHandler;
+import bgu.spl.net.srv.Connections;
+
+import java.net.DatagramSocket;
+import java.util.HashMap;
+
+public class TftpServer implements Connections<T> {
+    HashMap<Integer, ConnectionHandler> clientsMap;
+    DatagramSocket socket;
+    public TftpServer(){
+        clientsMap = new HashMap<>();
+        int port = Integer.parseInt(args[0]);
+        socket = new DatagramSocket(port);
+    }
+    @Override
+    public void connect(int connectionId, ConnectionHandler<T> handler) {
+        clientsMap.put(connectionId, handler);
+    }
+
+    @Override
+    public boolean send(int connectionId, T msg) {
+        ConnectionHandler currentClient = clientsMap.get(connectionId);
+        socket.send(T);
+
+        return false;
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+
+    }
+
     //TODO: Implement this
+    public void send(T msg){
+
+    }
+
 }
