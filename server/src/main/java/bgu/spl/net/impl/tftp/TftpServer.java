c67702ca1172b4af472762a29aa02f1938c00b97
orozen <62594754+orozen@users.noreply.github.com>
2024-03-16 10:45:59 +0200

    Update TftpServer.java

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -1,22 +1,15 @@
 package bgu.spl.net.impl.tftp;
 
-import bgu.spl.net.srv.ConnectionHandler;
-import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.Server;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.Socket;
-import java.net.SocketException;
-import java.util.HashMap;
-import java.util.Set;
 
 public class TftpServer{
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        LoggedUsers loggedUsers = new LoggedUsers();
+        String filesFolderPath = System.getProperty("user.dir") + "/server/Flies/";
+
         Server.threadPerClient(7777,
-                ()-> new TftpProtocol(),
+                ()-> new TftpProtocol(loggedUsers, filesFolderPath),
                 TftpEncoderDecoder::new
         ).serve();
 
